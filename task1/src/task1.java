dbce68350f8fda957de616dedf54182a277687f9
otpslasher <48891801+otpslasher@users.noreply.github.com>
2024-06-06 13:48:56 +0300

    Update task1.java

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -4,9 +4,6 @@ public class task1 {
 
     public static void main(String[] args) {
 
-
-        Scanner sc = new Scanner(System.in);
-
             int [] nums = new int[args.length];
         for (int i = 0; i < args.length; i++) {
             nums[i] = Integer.parseInt(args[i]);
